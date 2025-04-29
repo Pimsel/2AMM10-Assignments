295b10a1798aa19d1fa5847bc1f2cad101321a94
AfroJadocusKwak <pimdewildt11@gmail.com>
2025-04-30 01:11:17 +0200

    First round of comparisons
    Basic and Intermediate were clearly inferior. However, Advanced performs similarly with the full MobileNetV3, while it is quite a bit faster to train and seems to be much lighter as well.

--- train.py
+++ train.py
@@ -11,16 +11,18 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 import wandb
-import tqdm
+import time
+from tqdm import tqdm
 from models import *
 
 num_classes = 39
-model_dict = {
-    "BasicCNN": BasicCNN(num_classes=num_classes),
-    "IntermediateCNN": IntermediateCNN(num_classes=num_classes),
-    "AdvancedCNN": AdvancedCNN(num_classes=num_classes),
-    "MobileNetV3": MobileNetV3(num_classes=num_classes)
-}
+def get_model(name, num_classes):
+    return {
+        "BasicCNN": BasicCNN,
+        "IntermediateCNN": IntermediateCNN,
+        "AdvancedCNN": AdvancedCNN,
+        "MobileNetV3": MobileNetV3
+    }[name](num_classes=num_classes)
 
 # Retrieves data from local cache or downloads if not cached yet
 dataset_path = kagglehub.dataset_download("paramaggarwal/fashion-product-images-small")
@@ -53,6 +55,7 @@ class FashionDataset(Dataset):
         return image, self.targets[idx]
 
 transform = transforms.Compose([
+    transforms.Resize((80, 60)),
     transforms.ToTensor()
 ])
 
@@ -61,7 +64,7 @@ val_dataset = FashionDataset('dataset/presplit_train/valsplit.csv', img_dir, tra
 
 
 def train(config):
-    with wandb.init(project=config.model_name, config=config):
+    with wandb.init(project="ModelComparison2", config=config):  # Change to project=config.model_name when sweeping specific model later
         config = wandb.config
 
         # Prepare dataloaders
@@ -70,16 +73,27 @@ def train(config):
 
         # Initialize model, optimizer, and loss function (criterion)
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model = model_dict[config.model_name]
-        model = model.to(device)
-        optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
+        model = get_model(config.model_name, num_classes=num_classes).to(device)
         criterion = nn.CrossEntropyLoss()
+        if config.optimizer == "SGD":
+            optimizer = optim.SGD(model.parameters(), lr=config.learning_rate, weight_decay=1e-4)
+            scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer, 
+                mode='min',              # 'min' because we want to minimize val_loss
+                factor=0.5,              # LR reduced by a factor of 0.1
+                patience=4               # Wait 5 epochs before reducing
+            )
+        elif config.optimizer == "Adam":
+            optimizer = optim.Adam(model.parameters(), lr=config.learning_rate, weight_decay=1e-4)
 
         # Initialize patience variables
         best_val_loss = float("inf")
         patience_counter = 0
 
         for epoch in range(config.epochs):
+            if config.time_logging:
+                start_time = time.time()
+
             model.train()
             train_loss, train_correct, train_total = 0.0, 0, 0
 
@@ -115,11 +129,18 @@ def train(config):
             val_loss /= val_total
             val_acc = val_correct / val_total
 
+            if config.optimizer == "SGD":
+                scheduler.step(val_loss)
+            
+            if config.time_logging:
+                total_time = time.time() - start_time
+
             wandb.log({
                 'train_loss': train_loss,
                 'train_accuracy': train_acc,
                 'validation_loss': val_loss,
-                'validation_accuracy': val_acc
+                'validation_accuracy': val_acc,
+                'epoch_time': total_time if config.time_logging else 0
             })
 
             if val_loss < best_val_loss:
@@ -138,10 +159,12 @@ def train(config):
 # Initialize default hyperparameters and parser functionality
 default_config = SimpleNamespace(
     model_name="MobileNetV3",
-    batch_size=64,
+    batch_size=128,
+    optimizer="Adam",
     learning_rate=5e-4,
     epochs=500,
-    patience=5
+    patience=5,
+    time_logging=False
 )
 
 
@@ -149,14 +172,21 @@ def parse_args():
     argparser = argparse.ArgumentParser(description='Process hyperparameters')
     argparser.add_argument('--model_name', type=str, default=default_config.model_name, help='The name of the preferred model as specified in models.py')
     argparser.add_argument('--batch_size', type=int, default=default_config.batch_size, help='The batch size for the data loaders')
+    argparser.add_argument('--optimizer', type=str, default=default_config.optimizer, help='The optimizer to use for training')
     argparser.add_argument('--learning_rate', type=float, default=default_config.learning_rate, help='The learning rate for the Adam optimizer')
     argparser.add_argument('--epochs', type=int, default=default_config.epochs, help='The maximum number of epochs to run the training for')
     argparser.add_argument('--patience', type=int, default=default_config.patience, help='The number of epochs after which training is stopped if validation loss does not improve')
+    argparser.add_argument('--time_logging', type=bool, default=default_config.time_logging, help='Whether to log the time per epoch or not')
     args = argparser.parse_args()
     vars(default_config).update(vars(args))
     return
 
 
 if __name__ == '__main__':
-    parse_args()
-    train(default_config)
+    if wandb.run is not None:
+        # Meaning a sweep is active
+        train(wandb.config)  # ensure to declare all arguments in the yaml
+    else:
+        # Meaning a 'manual' run is done
+        parse_args()
+        train(default_config)
